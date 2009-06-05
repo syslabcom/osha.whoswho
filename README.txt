9c9fa6b7abd390c68e827e26aa33e4193eff4dd5
pilz <pilz@97d050b9-b799-4550-a901-4f04598d4601>
2009-06-05 13:46:57 +0000

    prepared for egg release 1.1

--- README.txt
+++ README.txt
@@ -1,7 +1,16 @@
-osha.whoswho Package Readme
-=========================
+.. contents::
 
-Overview
---------
+.. Note!
+   -----
+   
+   - code repository
+   - bug tracker
+   - questions/comments feedback mail
+
+
+- Code repository: http://svn.syslab.com/svn/OSHA/osha.whoswho
+- Questions and comments to info (at) syslab (dot) com
+- Report bugs at http://products.syslab.com/products/osha.whoswho/issues
+
 
 
--- setup.py
+++ setup.py
@@ -1,23 +1,51 @@
-from setuptools import setup, find_packages
+# -*- coding: utf-8 -*-
+"""
+This module contains the osha.whoswho package
+"""
 import os
+from setuptools import setup, find_packages
 
-version = '0.1'
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+version = '1.1'
+
+long_description = (
+    read('README.txt')
+    + '\n' +
+    'Change history\n'
+    '**************\n'
+    + '\n' +
+    read('CHANGES.txt')
+    + '\n' +
+    'Detailed Documentation\n'
+    '**********************\n'
+    + '\n' +
+    read('osha', 'whoswho', 'README.txt')
+    + '\n' +
+    'Contributors\n'
+    '************\n'
+    + '\n' +
+    read('CONTRIBUTORS.txt')
+    + '\n' 
+    )
+    
+tests_require=['zope.testing']1'
 
 setup(name='osha.whoswho',
       version=version,
       description="",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      long_description=long_description,
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
-      keywords='',
-      author='syslab.com',
+      keywords='osha contenttype whoswho belgium',
+      author='Syslab.com GmbH',
       author_email='info@syslab.com',
-      url='http://svn.plone.org/svn/plone/plone.example',
+      url='https://svn.syslab.com/svn/OSHA/osha.whoswho/',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['osha'],
@@ -27,7 +55,12 @@ setup(name='osha.whoswho',
           'setuptools',
           # -*- Extra requirements: -*-
       ],
+      tests_require=tests_require,
+      extras_require=dict(tests=tests_require),
+      test_suite = 'osha.whoswho.tests.test_docs.test_suite',
       entry_points="""
-      # -*- Entry points: -*-
+      # -*- entry_points -*- 
       """,
-      )
+      paster_plugins = ["ZopeSkel"],
+      )      
+      
